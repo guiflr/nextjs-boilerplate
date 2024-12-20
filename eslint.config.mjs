92d16096382f80d53eed1ced1f33985611b5636f
guilherme <guiflr97@gmail.com>
2024-12-20 09:51:11 -0300

    chore: config lint, prettier and husky

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,16 +1,24 @@
-import { dirname } from "path";
-import { fileURLToPath } from "url";
-import { FlatCompat } from "@eslint/eslintrc";
-
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = dirname(__filename);
+import js from '@eslint/js'
+import { FlatCompat } from '@eslint/eslintrc'
 
 const compat = new FlatCompat({
-  baseDirectory: __dirname,
-});
+  // import.meta.dirname is available after Node.js v20.11.0
+  baseDirectory: import.meta.dirname,
+  recommendedConfig: js.configs.recommended
+})
 
 const eslintConfig = [
-  ...compat.extends("next/core-web-vitals", "next/typescript"),
-];
+  ...compat.config({
+    extends: ['eslint:recommended', 'next', 'prettier'],
+    rules: {
+      'no-undef': 'off',
+      'react-hooks/rules-of-hooks': 'error',
+      'react-hooks/exhaustive-deps': 'warn',
+      'react/prop-types': 'off',
+      'react/react-in-jsx-scope': 'off',
+      '@typescript-eslint/explit-module-boundary-types': 'off'
+    }
+  })
+]
 
-export default eslintConfig;
+export default eslintConfig
